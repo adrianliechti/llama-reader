d631160b56308d68e6d67eb64765a167644d9bdf
Adrian Liechti <adrian@monobox.ch>
2024-10-12 19:11:04 +0200

    wip

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,14 @@ FROM python:3.12-slim
 
 WORKDIR /app
 
+ENV HF_HOME=/app/.cache/huggingface
+ENV HF_HUB_VERBOSITY=error
+ENV HF_HUB_DISABLE_TELEMETRY=1
+ENV HF_HUB_DISABLE_PROGRESS_BARS=1
+
 COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir --extra-index-url https://download.pytorch.org/whl/cpu -r requirements.txt
+
 RUN python -m playwright install chromium --with-deps
 
 COPY main.py .
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import re
+import torch
 import uvicorn
 import html2text
 
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException, Header, Response
-from markdownify import markdownify as md
-from playwright.async_api import async_playwright, Playwright
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from playwright.sync_api import sync_playwright
+
+if torch.backends.mps.is_available():
+    device = 'mps'
+elif torch.cuda.is_available():
+    device = 'cuda'
+else:
+    device = 'cpu' 
+
+md_model = "jinaai/reader-lm-0.5b"
+md_tokenizer = AutoTokenizer.from_pretrained(md_model)
+md_model = AutoModelForCausalLM.from_pretrained(md_model).to(device)
+
+md_model.eval()
 
 app = FastAPI(
     title="LLM Platform Reader"
@@ -17,40 +31,48 @@ class ReadRequest(BaseModel):
     format: str = "text"
 
 @app.get("/{url:path}")
-async def read_get(url: str, x_return_format: Optional[str] = Header('text')):
+def read_get(url: str, x_return_format: Optional[str] = Header('text')):
     url = url.lstrip('/')
     format = x_return_format
     
-    return await read(url, format)
+    return read(url, format)
 
 @app.post("/")
-async def read(request: ReadRequest, x_return_format: Optional[str] = Header(None)):
+def read_post(request: ReadRequest, x_return_format: Optional[str] = Header(None)):
     url = request.url
     format = request.format
     
     if x_return_format:
         format = x_return_format
         
-    return await read(url, format)
+    return read(url, format)
 
-async def read(url, format="text"):
+def read(url, format="text"):
     if not url.startswith(('http://', 'https://')):
         url = 'https://' + url
         
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
 
         try:
-            page = await browser.new_page()
-            await page.goto(url, wait_until='networkidle')
+            page = browser.new_page()
+            page.goto(url, wait_until='networkidle')
 
             match format:
+                case "html":
+                    content = page.content()
+                    return Response(content=content, media_type='text/html')
+                
+                case "pdf":
+                    content = page.pdf()
+                    return Response(content=content, media_type='application/pdf')
+                
                 case "text":
                     h = html2text.HTML2Text()
                     h.ignore_links = True
                     h.ignore_images = True
                     
-                    content = await page.content()
+                    content = page.content()
                     content = h.handle(content)
                     
                     content = re.sub(r'^\s*\d+\s*$', '', content, flags=re.MULTILINE)
@@ -59,16 +81,17 @@ async def read(url, format="text"):
                     return Response(content=content, media_type='text/markdown')
                 
                 case "markdown":
-                    content = md(await page.content(), heading_style='ATX')
-                    return Response(content=content, media_type='text/markdown')
-                 
-                case "html":
-                    content = await page.content()
-                    return Response(content=content, media_type='text/html')
-                 
-                case "pdf":
-                    content = await page.pdf()
-                    return Response(content=content, media_type='application/pdf')
+                    content = page.content()
+
+                    messages = [{"role": "user", "content": content}]
+                    input_text=md_tokenizer.apply_chat_template(messages, tokenize=False)
+
+                    inputs = md_tokenizer.encode(input_text, return_tensors="pt").to(device)
+                    outputs = md_model.generate(inputs, max_new_tokens=1024, temperature=0, do_sample=False, repetition_penalty=1.08)
+
+                    output_text=md_tokenizer.decode(outputs[0])
+
+                    return Response(content=output_text, media_type='text/markdown')
                  
                 case _:
                     raise HTTPException(status_code=400, detail="Invalid format")
@@ -76,9 +99,8 @@ async def read(url, format="text"):
         except Exception as e:
                 raise HTTPException(status_code=500, detail=str(e))
             
-
         finally:
-            await browser.close()
+            browser.close()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
